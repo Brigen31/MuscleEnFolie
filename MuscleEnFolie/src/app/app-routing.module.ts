79acda1fd4fbdb8873d8133f5d8735075ddd4e30
julescavanier <jules.cavanier@ynov.com>
2024-05-27 16:27:00 +0200

    firebase not work

--- MuscleEnFolie/src/app/app-routing.module.ts
+++ MuscleEnFolie/src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginPageComponent } from './login-page/login-page.component';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginPageComponent },
 ];
 
 @NgModule({
--- MuscleEnFolie/src/app/app.module.ts
+++ MuscleEnFolie/src/app/app.module.ts
@@ -1,19 +1,39 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginDialogComponent } from './login-dialog/login-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { SignupComponent } from './signup/signup.component'; // importez MatCardModule
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { firebaseConfig } from './env/environement';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginDialogComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([]), // ajoutez vos routes ici
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule, // ajoutez MatCardModule à la liste des imports
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule],
   providers: [],
   bootstrap: [AppComponent]
 })
